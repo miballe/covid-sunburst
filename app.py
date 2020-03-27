0b0ba4a1b948db5b6d361428a53a707bf264b126
Miguel Ballesteros <miballegh@outlook.com>
2020-03-28 00:06:50 +0100

    Basic structure

---
+++ app.py
@@ -0,0 +1,70 @@
+import numpy as np
+import pandas as pd
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+from dash.dependencies import Input, Output
+
+
+def industries_hierarchy() -> pd.DataFrame:
+    ret_ind = pd.read_csv('industries-hrchy.csv')
+    ret_ind = ret_ind.replace(np.nan, '', regex=True)
+    return ret_ind
+
+industries_hrchy = industries_hierarchy()
+industry_sentiment = pd.read_json('covidsm_agg_sentiment_industry.json.zip', orient='records')
+
+external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+
+app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+
+app.layout = html.Div([
+    dcc.Graph(id='graph-with-slider'),
+    dcc.Slider(
+        id='day-slider',
+        min=industry_sentiment['published_at_date'].min(),
+        max=industry_sentiment['published_at_date'].max(),
+        value=industry_sentiment['published_at_date'].min(),
+        marks={df['published_at_date'].unique()},
+        step=None
+    )
+])
+
+
+@app.callback(
+    Output('graph-with-slider', 'figure'),
+    [Input('year-slider', 'value')])
+def update_figure(selected_year):
+    filtered_df = df[df.year == selected_year]
+    traces = []
+    for i in filtered_df.continent.unique():
+        df_by_continent = filtered_df[filtered_df['continent'] == i]
+        traces.append(dict(
+            x=df_by_continent['gdpPercap'],
+            y=df_by_continent['lifeExp'],
+            text=df_by_continent['country'],
+            mode='markers',
+            opacity=0.7,
+            marker={
+                'size': 15,
+                'line': {'width': 0.5, 'color': 'white'}
+            },
+            name=i
+        ))
+
+    return {
+        'data': traces,
+        'layout': dict(
+            xaxis={'type': 'log', 'title': 'GDP Per Capita',
+                   'range':[2.3, 4.8]},
+            yaxis={'title': 'Life Expectancy', 'range': [20, 90]},
+            margin={'l': 40, 'b': 40, 't': 10, 'r': 10},
+            legend={'x': 0, 'y': 1},
+            hovermode='closest',
+            transition = {'duration': 500},
+        )
+    }
+
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
