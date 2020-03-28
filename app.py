c79e97ad790c02f2e86420eab31c208cb512c647
Miguel Ballesteros <miballegh@outlook.com>
2020-03-28 22:52:24 +0100

    Ploted a Sunburst within Dash

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import pandas as pd
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
+import plotly.graph_objects as go
 from dash.dependencies import Input, Output
 
 
@@ -11,59 +12,53 @@ def industries_hierarchy() -> pd.DataFrame:
     ret_ind = ret_ind.replace(np.nan, '', regex=True)
     return ret_ind
 
+
 industries_hrchy = industries_hierarchy()
 industry_sentiment = pd.read_json('covidsm_agg_sentiment_industry.json.zip', orient='records')
+
+fig_layout = dict(margin=dict(t=0, l=0, r=0, b=0), width=800, height=850)
+
+fig_ind = go.Figure(data=[go.Sunburst(
+        ids=industries_hrchy['ind_fcode'],
+        labels=industries_hrchy['name'],
+        parents=industries_hrchy['parent']
+        # ,values=industries_hrchy['values']
+    )],
+    layout=fig_layout
+)
 
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
 app.layout = html.Div([
-    dcc.Graph(id='graph-with-slider'),
+    dcc.Graph(id='sunburst-with-slider', figure=fig_ind),
     dcc.Slider(
         id='day-slider',
-        min=industry_sentiment['published_at_date'].min(),
-        max=industry_sentiment['published_at_date'].max(),
-        value=industry_sentiment['published_at_date'].min(),
-        marks={df['published_at_date'].unique()},
-        step=None
+        min=1,
+        max=71,
+        value=1,
+        marks={i: '{}'.format(i) for i in range(71)}
     )
 ])
 
 
 @app.callback(
-    Output('graph-with-slider', 'figure'),
-    [Input('year-slider', 'value')])
-def update_figure(selected_year):
-    filtered_df = df[df.year == selected_year]
-    traces = []
-    for i in filtered_df.continent.unique():
-        df_by_continent = filtered_df[filtered_df['continent'] == i]
-        traces.append(dict(
-            x=df_by_continent['gdpPercap'],
-            y=df_by_continent['lifeExp'],
-            text=df_by_continent['country'],
-            mode='markers',
-            opacity=0.7,
-            marker={
-                'size': 15,
-                'line': {'width': 0.5, 'color': 'white'}
-            },
-            name=i
-        ))
+    Output('sunburst-with-slider', 'figure'),
+    [Input('day-slider', 'value')])
+def update_figure(selected_date):
+    # filtered_df = industry_sentiment[industry_sentiment['published_at_date'] == selected_date]
 
-    return {
-        'data': traces,
-        'layout': dict(
-            xaxis={'type': 'log', 'title': 'GDP Per Capita',
-                   'range':[2.3, 4.8]},
-            yaxis={'title': 'Life Expectancy', 'range': [20, 90]},
-            margin={'l': 40, 'b': 40, 't': 10, 'r': 10},
-            legend={'x': 0, 'y': 1},
-            hovermode='closest',
-            transition = {'duration': 500},
-        )
-    }
+    # fig_ind_layout = dict(margin=dict(t=0, l=0, r=0, b=0), width=600, height=650)
+
+    return go.Figure(data=[go.Sunburst(
+                    ids=industries_hrchy['ind_fcode'],
+                    labels=industries_hrchy['name'],
+                    parents=industries_hrchy['parent']
+                    # ,values=industries_hrchy['values']
+                )],
+                layout=fig_layout
+            )
 
 
 if __name__ == '__main__':
